69fa78b2540029a74559bf30ad1256a7c84597cf
Valeri Riazanski <kot-aldo@yandex.ru>
2021-04-20 01:23:47 +0400

    commit 5 после log out попадаем сразу на log in

--- vsearch.py
+++ vsearch.py
@@ -6,5 +6,19 @@ Created on Sat Mar 20 02:08:16 2021
 """
 
 
-def search4letters(phrase: str, letters: str = 'aeiou') -> set:
-    return set(letters).intersection(set(phrase))
+def search4letters(phrase: str, letters: str = 'aeiou') -> str:
+    list_phrase = list(phrase)
+    list_letters = list(letters)
+    frequency = {}
+    for ch in list_letters:
+        frequency.setdefault(ch, 0)
+    for l in list_phrase:
+        if l in list_letters:
+            frequency[l] += 1
+    list_freq = []
+    for k, v in sorted(frequency.items()):
+        list_freq.extend([k, '=', str(v), ' '])
+    return ''.join(list_freq)
+
+
+    # return set(letters).intersection(set(phrase))
